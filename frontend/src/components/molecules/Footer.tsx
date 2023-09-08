e51d944d002144789be27a284552a70a9d45147d
theody85 <gyambrahtheodora.8@gmail.com>
2023-09-08 04:33:19 +0000

    add changes to footer

--- frontend/src/components/molecules/Footer.tsx
+++ frontend/src/components/molecules/Footer.tsx
@@ -6,21 +6,25 @@ import { Button } from "../ui/button";
 const Footer = () => {
   return (
     <div className="flex flex-col">
-      <div className="bg-[#252B42] py-32 flex flex-col items-center text-center z-10 ">
-        <h2 className="font-mono text-white  ">
-          Every donation makes an impact
-        </h2>
-        <h3 className="text-[#BDBDBD] font-semibold ">
-          For Charities and Grant donor organizations
-        </h3>
-        <div className="flex gap-5 mt-10">
-          <Button className="bg-[#471AA0] px-6 hover:bg-black shadow-md">
-            Donate now
-          </Button>
-          <Button className="bg-[#252B42] border px-6 hover:bg-black shadow-md ">
-            Register for free
-          </Button>
-        </div>
+      <div className="bg-[#252B42]  ">
+        <Section>
+          <div className="py-32 flex flex-col items-center text-center z-10">
+            <h2 className="font-mono text-white  ">
+              Every donation makes an impact
+            </h2>
+            <h3 className="text-[#BDBDBD] font-semibold ">
+              For Charities and Grant donor organizations
+            </h3>
+            <div className="flex gap-5 mt-10">
+              <Button className="bg-[#471AA0] px-6 hover:bg-black shadow-md">
+                Donate now
+              </Button>
+              <Button className="bg-[#252B42] border px-6 hover:bg-black shadow-md ">
+                Register for free
+              </Button>
+            </div>
+          </div>
+        </Section>
       </div>
       <Section>
         <div className="flex flex-col md:flex-row items-center justify-between gap-6 ">
