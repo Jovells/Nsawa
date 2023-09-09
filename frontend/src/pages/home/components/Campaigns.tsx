e824f91189391c5a8b1fbee7b9563a722b9110b2
theody85 <gyambrahtheodora.8@gmail.com>
2023-09-09 10:23:05 +0000

    add ui changes

--- frontend/src/pages/home/components/Campaigns.tsx
+++ frontend/src/pages/home/components/Campaigns.tsx
@@ -2,13 +2,19 @@
 import Section from "@/components/layouts/Section";
 import Campaign from "@/pages/campaigns/components/Campaigns";
 import useQueryCampaigns from "@/pages/campaigns/hooks/useQueryCampaigns";
+import { Link } from "react-router-dom";
 
 const NsawaCampaigns = () => {
   const { campaigns } = useQueryCampaigns();
   return (
     <Section>
       <div className="md:mb-40 mx-10 mb-20 flex-col justify-center">
-        <h2 className="text-center"># Nsawa Campaigns</h2>
+        <Link to="/campaigns">
+          <h2 className="md:text-center text-[#471AA0] cursor-pointer ">
+            # Nsawa Campaigns
+          </h2>
+        </Link>
+
         {/* @ts-ignore */}
         <Campaign campaigns={campaigns} />
       </div>
