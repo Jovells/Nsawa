2d4fe1d53636bd7dd9f4db693b6e7167589eed61
theody85 <gyambrahtheodora.8@gmail.com>
2023-09-09 10:20:28 +0000

    add ui changes

--- frontend/src/pages/campaigns/components/Campaigns.tsx
+++ frontend/src/pages/campaigns/components/Campaigns.tsx
@@ -9,7 +9,7 @@ const Campaign = ({ campaigns }: { campaigns: [] }) => {
       <div className="flex flex-wrap justify-center gap-16  my-12">
         {campaigns.map((campaign, index) => (
           <div
-            className="flex flex-col shadow-xl w-[300px] h-[400px] rounded-xl"
+            className="flex flex-col shadow-xl w-[300px] h-[450px] rounded-xl"
             key={index}
           >
             <img src={avatar} alt="image" />
@@ -21,6 +21,9 @@ const Campaign = ({ campaigns }: { campaigns: [] }) => {
               <p className="h-[120px]">{campaign.description}</p>
               {/* @ts-ignore */}
               <p className="font-bold">GHS {campaign.amountReceived}</p>
+              <div className="bg-[#cfc0ea] p-1 w-fit rounded-lg my-2">
+                Total amount raised
+              </div>
 
               <Button className="w-full bg-[#471AA0] my-4">Donate</Button>
             </div>
